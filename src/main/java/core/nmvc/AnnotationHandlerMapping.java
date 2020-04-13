e938da82b670e9439e1fb09f6756fd9caa9ad6b0
Dongsun Choi <movinglinecheck@gmail.com>
2020-04-13 12:12:00 +0900

    애노테이션 기반 mvc 프레임워크 구현 (#2)
    - Reflection을 활용해 @Controller 달린 클래스와 @RequestMapping 달린 메소드를 스캔하도록 구현
    - 스캔한 정보를 바탕으로 요청에 응답할 수 있는 AnnotationHandlerMapping 구현

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,27 +1,51 @@
 package core.nmvc;
 
 import com.google.common.collect.Maps;
+import core.annotation.RequestMapping;
 import core.annotation.RequestMethod;
+import org.reflections.ReflectionUtils;
 import was.http.HttpRequest;
 
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
     private Object[] basePackage;
-
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
     }
 
-    public void initialize() {
+    public void initialize() throws IllegalAccessException, InstantiationException {
+        ControllerScanner sc = new ControllerScanner(basePackage);
+        Map<Class<?>, Object> controllers = sc.getControllers();
 
+        for (Class<?> aClass : controllers.keySet()) {
+            Set<Method> methods = ReflectionUtils.getAllMethods(aClass, ReflectionUtils.withAnnotation(RequestMapping.class));
+
+            for (Method method : methods) {
+                RequestMapping rm = method.getAnnotation(RequestMapping.class);
+                HandlerKey handlerKey = createHandlerKey(rm);
+                HandlerExecution handlerExecution = createHandlerExecution(aClass.newInstance(), method);
+
+                handlerExecutions.put(handlerKey, handlerExecution);
+            }
+        }
     }
 
     public HandlerExecution getHandler(HttpRequest request) {
         String requestUri = request.getPath();
         RequestMethod rm = RequestMethod.valueOf(request.getMethod().toString());
         return handlerExecutions.get(new HandlerKey(requestUri, rm));
+}
+
+    private HandlerKey createHandlerKey(RequestMapping rm) {
+        return new HandlerKey(rm.value(), rm.method());
+    }
+
+    private HandlerExecution createHandlerExecution(Object declaredObject, Method method) {
+        return new HandlerExecution(declaredObject, method);
     }
 }
--- src/main/java/core/nmvc/HandlerExecution.java
+++ src/main/java/core/nmvc/HandlerExecution.java
@@ -3,8 +3,18 @@ package core.nmvc;
 import was.http.HttpRequest;
 import was.http.HttpResponse;
 
+import java.lang.reflect.Method;
+
 public class HandlerExecution {
-    public String handle(HttpRequest request, HttpResponse response) throws Exception {
-        return null;
+    private Object declaredObject;
+    private Method method;
+
+    public HandlerExecution(Object declaredObject, Method method) {
+        this.declaredObject = declaredObject;
+        this.method = method;
+    }
+
+    public Object handle(HttpRequest request, HttpResponse response) throws Exception {
+        return method.invoke(declaredObject, request, response);
     }
 }
